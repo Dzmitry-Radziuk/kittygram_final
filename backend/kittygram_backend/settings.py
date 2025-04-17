9f9e39c7a01f1b34905ae6947e72d83ea752d83b
Dzmitry-Radziuk <mitia.radiuk@yandex.ru>
2025-04-17 21:18:37 +0300

    Set DEBUG to False in settings.py for production readiness

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 if not SECRET_KEY:
     raise ValueError("Невозможно получить SECRET_KEY из переменной окружения...")
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost 127.0.0.1').split()
 
